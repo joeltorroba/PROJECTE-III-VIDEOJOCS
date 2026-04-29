f07c66564d833ad4fa422fdb0992683cff923e14
adrianhonrubia <adrian.honrubia@estudiantat.upc.edu>
2026-04-29 17:33:35 +0200

    barra de vida

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,20 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float maxHealth = 100f;
     public float currentHealth;
 
+    [Header("UI")]
+    public Slider healthBar;
+
     void Start()
     {
         currentHealth = maxHealth;
+
+        if (healthBar != null)
+        {
+            healthBar.maxValue = maxHealth;
+            healthBar.value = currentHealth;
+        }
     }
 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
 
-        Debug.Log("Vida: " + currentHealth);
+        UpdateHealthUI();
 
         if (currentHealth <= 0)
         {
@@ -22,9 +33,16 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
+    void UpdateHealthUI()
+    {
+        if (healthBar != null)
+        {
+            healthBar.value = currentHealth;
+        }
+    }
+
     void Die()
     {
-        // Llama al GameOver
         GameManager.instance.GameOver();
     }
 }
