627d20348b06cb88648a7722bafc5014c44bca3c
Randomyura <jorge.sunyer.guirado@estudiantat.upc.edu>
2026-05-29 14:08:51 +0200

    cambios y arreglos

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Pantallas")]
     public GameObject menuPrincipal;
     public GameObject personalizar;
 
     public void PlayGame()
     {
-        // Tu código
+        SceneManager.LoadScene("GameScene");
     }
 
     public void OpenPersonalizar()
