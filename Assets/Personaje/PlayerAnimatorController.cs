87a58af1d7971f1bd471ea3d2048f6d9385ed784
joeltorroba <joel.torroba@estudiantat.upc.edu>
2026-05-31 00:05:54 +0200

    arreglo animacion

--- Assets/Personaje/PlayerAnimatorController.cs
+++ Assets/Personaje/PlayerAnimatorController.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using System.Collections; // Necesario para usar Coroutines
 
 public class PlayerAnimationController : MonoBehaviour
 {
     Animator anim;
+    private Coroutine speedResetCoroutine;
 
     void Start()
     {
@@ -12,12 +14,35 @@ public class PlayerAnimationController : MonoBehaviour
     public void SetFalling()   { anim.SetTrigger("Falling"); }
     public void SetPropulse()  { anim.SetTrigger("Propulse"); }
     public void SetLand()      { anim.SetTrigger("Land"); }
-    public void SetHit()       { anim.SetTrigger("Hit"); }
     public void SetDie()       { anim.SetTrigger("Die"); }
 
-    // Llamado por Animation Event al final de FallFlatImpact
-    public void OnDieAnimFinished()
+    // Función modificada con temporizador automático por código
+    public void SetHit(float tiempoEfecto)
     {
-        GameManager.instance.GameOver();
+        anim.SetTrigger("Hit");
+
+        // Obtenemos la duración real en segundos de tu clip en Blender
+        float duracionClipOriginal = anim.GetCurrentAnimatorStateInfo(0).length;
+
+        if (duracionClipOriginal > 0 && tiempoEfecto > 0)
+        {
+            anim.speed = duracionClipOriginal / tiempoEfecto;
+        }
+
+        // Si ya había un temporizador contando, lo paramos para empezar de nuevo
+        if (speedResetCoroutine != null)
+        {
+            StopCoroutine(speedResetCoroutine);
+        }
+
+        // Iniciamos la cuenta atrás para devolver la velocidad a la normalidad
+        speedResetCoroutine = StartCoroutine(ResetSpeedAfterTime(tiempoEfecto));
+    }
+
+    // Esta rutina espera a que el objeto desaparezca y resetea la velocidad
+    IEnumerator ResetSpeedAfterTime(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        anim.speed = 1f;
     }
 }
